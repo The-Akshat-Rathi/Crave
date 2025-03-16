2d9e1e3b634b7cb727721a4f5b1cc11444226497
The-Akshat-Rathi <akshat.rathi.ajmer.pn@gmail.com>
2025-03-16 14:37:17 +0530

    first

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {useLocalSearchParams} from "expo-router";
+
+const properties = () => {
+    const { id } = useLocalSearchParams();
+    return (
+        <View>
+            <Text>properties{ id }</Text>
+        </View>
+    )
+}
+export default properties
